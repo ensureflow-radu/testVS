96e60e7f21460f1aff9c08f46c8bc910dc929b1c
Radu Cosoveanu <radu.cosoveanu@ensureflow.com>
2019-09-09 15:12:57 +0800

    Add data access project to solution

---
+++ ConsoleApp1/ClassLibrary.DataAccess/CoffeeShopDataProvider.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary.DataAccess.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary.DataAccess
+{
+    public class CoffeeShopDataProvider
+    {
+        public IEnumerable<CoffeeShop> LoadCoffessShops()
+        {
+            yield return new CoffeeShop { Location = "Frankfurt", BeansInStockInKg = 107 };
+            yield return new CoffeeShop { Location = "Freiburg", BeansInStockInKg = 23 };
+            yield return new CoffeeShop { Location = "Munich", BeansInStockInKg = 56};
+        }
+    }
+}
---
+++ ConsoleApp1/ClassLibrary.DataAccess/Model/CoffeeShop.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary.DataAccess.Model
+{
+    public class CoffeeShop
+    {
+        public string Location { get; set; }
+        public int BeansInStockInKg { get; set; }
+    }
+}
